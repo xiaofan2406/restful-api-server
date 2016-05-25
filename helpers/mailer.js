c5da7186019b9941056600a576c85b3db145ba23
Xiaofan Wu <xiaofan.wu@eeaust.com.au>
2016-05-25 15:46:45 +1000

    update mailer

--- helpers/mailer.js
+++ helpers/mailer.js
@@ -4,17 +4,19 @@ const mailConfig = require('../config/mail-config');
 // create reusable transporter object using the default SMTP transport
 const transporter = nodemailer.createTransport(mailConfig);
 
-
-module.exports =
-(to, subject, content, fromAddress = '"Admin RestAPI Server" <admin@restfulapi.com>') => {
-  // setup e-mail data with unicode symbols
-  const mailOptions = {
-    from: fromAddress, // sender address
-    to,
-    subject,
-    html: content
-  };
-
+/**
+ * mailOptions:
+ * from 'sender@server.com' or formatted '"Sender Name" <sender@server.com>'
+ * to - Comma separated list or an array of recipients e-mail addresses
+ * cc - Comma separated list or an array of recipients e-mail addresses
+ * bcc - Comma separated list or an array of recipients e-mail addresses
+ * subject - The subject of the e-mail
+ * text - The plaintext version of the message
+ * html - The HTML version of the message
+ * attachments - An array of attachment objects
+ * https://github.com/nodemailer/nodemailer for more details
+ */
+module.exports = (mailOptions) => {
   return new Promise((resolve, reject) => {
     // send mail with defined transport object
     transporter.sendMail(mailOptions, (error, info) => {
