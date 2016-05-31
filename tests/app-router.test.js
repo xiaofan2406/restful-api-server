e295e3fe9fbceab380fdfa384bab4acd8e69a606
Xiaofan Wu <xiaofan.wu@eeaust.com.au>
2016-05-31 15:51:19 +1000

    testing update

--- tests/app-router.test.js
+++ tests/app-router.test.js
@@ -1,106 +1,451 @@
 const supertest = require('supertest');
 const expect = require('chai').expect;
 const bcrypt = require('bcrypt-nodejs');
-
+const axios = require('axios');
 const { User } = require('../models');
 
-const api = supertest('http://localhost:3000');
+const { SERVER_URL } = require('../config/app-config');
 
-describe('Authentication', function() {
-  const password = 'password';
-  const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10), null);
+describe('/signUp', function() {
+  const correctEmail = 'test@mail.com';
 
-  describe('Successful Sign Up', function() {
-    let newUser;
-    let beforeCount;
-    let userRes;
-    const email = 'test@mail.com';
+  describe('with correct request data', function() {
+    let errorRes, response;
+    const email = correctEmail;
+    const password = 'password';
+    const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10), null);
 
     before(function(done) {
-      User.count().then(count => { // get the initial count of users
-        beforeCount = count;
-        api.post('/signup') // send the request to sign up a new user
-        .send({
-          email,
-          password
-        })
-        .end((err, res) => {
-          userRes = res; // store the response send back by server
-          User.find({ where: { email }}).then(user => { // find the user in database
-            newUser = user;
-            done();
-          });
-        });
+      axios.post(`${SERVER_URL}/signUp`, { email,  password })
+      .then(res => {
+        response = res;
+        done();
+      })
+      .catch(err => {
+        errorRes = err;
+        done(err);
       });
     });
 
     it('should create a new entry in the database', function(done) {
-      User.count().then(count => {
-        expect(count).to.equal(beforeCount + 1);
-        done();
-      });
-    });
-
-    it('should encrypt password in the database', function(done) {
-      bcrypt.compare(password, newUser.password, (err, isMatch) => {
-        if (err) {
-          throw err;
-        }
-        expect(isMatch).to.be.true;
-        done();
-      });
-    });
-
-    it('should return the user information', function() {
-      expect(userRes.status).to.equal(201);
-      expect(userRes.body.email).to.equal(email);
-      expect(userRes.body).to.have.ownProperty('token');
-    });
-
-    after(function() {
-
-      newUser.destroy();
-    });
-  });
-
-  describe('Successful Sign Up', function() {
-    let newUser;
-    let beforeCount;
-    let userRes;
-    const email = "@mail.com";
-    before(function(done) {
-      User.count().then(count => { // get the initial count of users
-        beforeCount = count;
-        api.post('/signup') // send the request to sign up a new user
-        .send({
-          email,
-          password
-        })
-        .end((err, res) => {
-          userRes = res; // store the response send back by server
-          User.find({ where: { email }}).then(user => { // find the user in database
-            newUser = user;
-            done();
-          });
+      User.findByEmail(email).then(user => {
+        expect(user).to.exist;
+        expect(user.email).to.equal(email);
+        expect(user.displayName).to.equal(email);
+        expect(user.UUID).to.exist;
+        expect(user.activated).to.be.false;
+        bcrypt.compare(password, user.password, (err, isMatch) => {
+          if (err) {
+            return done(err);
+          }
+          expect(isMatch).to.be.true;
+          done();
         });
       });
     });
 
-    it('should NOT create a new entry in the database', function(done) {
-      User.count().then(count => {
-        expect(count).to.equal(beforeCount);
+    it('should return the user displayName', function() {
+      expect(response.status).to.equal(202);
+      expect(response.data.displayName).to.equal(email);
+    });
+  });
+
+  describe('with mal-formed request data', function() {
+
+  });
+
+  describe('with wrong email format', function() {
+    let errorRes, response;
+    const email = 'email';
+    const password = 'password';
+
+    before(function(done) {
+      axios.post(`${SERVER_URL}/signUp`, { email,  password })
+      .then(res => {
+        response = res;
+        done();
+      })
+      .catch(err => {
+        errorRes = err;
         done();
       });
     });
 
-    it('should NOT return any user information', function() {
+    it('should return an 422 error', function() {
+      expect(errorRes.status).to.equal(422);
+    });
+  });
 
-      expect(newUser).to.be.null;
+  describe('with empty email', function() {
+    let errorRes, response;
+    const email = '';
+    const password = 'password';
+
+    before(function(done) {
+      axios.post(`${SERVER_URL}/signUp`, { email, password })
+      .then(res => {
+        response = res;
+        done();
+      })
+      .catch(err => {
+        errorRes = err;
+        done();
+      });
     });
 
-    it('should return response code 400', function() {
-      expect(userRes.status).to.equal(400);
+    it('should return an 422 error', function() {
+      expect(errorRes.status).to.equal(422);
     });
+  });
+
+  describe('with no password', function() {
+    let errorRes, response;
+    const email = 'email@mail.com';
+    const password = '';
+
+    before(function(done) {
+      axios.post(`${SERVER_URL}/signUp`, { email, password })
+      .then(res => {
+        response = res;
+        done();
+      })
+      .catch(err => {
+        errorRes = err;
+        done();
+      });
+    });
+
+    it('should return an 422 error', function() {
+      expect(errorRes.status).to.equal(422);
+    });
+  });
+
+  describe('duplicate account', function() {
+    let errorRes, response;
+    const email = correctEmail;
+    const password = 'password';
+
+    before(function(done) {
+      axios.post(`${SERVER_URL}/signUp`, { email, password })
+      .then(res => {
+        response = res;
+        done();
+      })
+      .catch(err => {
+        errorRes = err;
+        done();
+      });
+    });
+
+    it('should return an 500 error', function() {
+      expect(errorRes.status).to.equal(500);
+    });
+  });
+
+  after(function(done) {
+    User.findByEmail(correctEmail).then(user => {
+      user.destroy();
+      done();
+    });
+  });
+
+});
+
+describe('/activateAccount', function() {
+  let newUser;
+  const correctEmail = 'test@mail.com';
+
+  before(function(done) {
+    axios.post(`${SERVER_URL}/signUp`, {
+      email: correctEmail,
+      password: 'password'
+    })
+    .then(res => {
+      return User.findByEmail(correctEmail);
+    })
+    .then(user => {
+      newUser = user;
+      done();
+    })
+    .catch(err => {
+      done(err);
+    });
+  });
+
+  describe('with non-existing email', function() {
+    let errorRes, response;
+    const email = 'non-existing@mail.com';
+
+    before(function(done) {
+      const hash = newUser.UUID;
+      axios.patch(`${SERVER_URL}/activateAccount`, { email, hash })
+      .then(res => {
+        response = res;
+        done();
+      })
+      .catch(err => {
+        errorRes = err;
+        done();
+      });
+    });
+
+    it('should return an 422 error', function() {
+      expect(errorRes.status).to.equal(422);
+    });
+  });
+
+  describe('with non-matching hash', function() {
+    let errorRes, response;
+    const email = correctEmail;
+
+    before(function(done) {
+      const hash = 'some non-matching hash';
+      axios.patch(`${SERVER_URL}/activateAccount`, { email, hash })
+      .then(res => {
+        response = res;
+        done();
+      })
+      .catch(err => {
+        errorRes = err;
+        done();
+      });
+    });
+
+    it('should return an 401 error', function() {
+      expect(errorRes.status).to.equal(401);
+    });
+  });
+
+  describe('with correct request data', function() {
+    let errorRes, response;
+    const email = correctEmail;
+
+    before(function(done) {
+      const hash = newUser.UUID;
+      axios.patch(`${SERVER_URL}/activateAccount`, { email, hash })
+      .then(res => {
+        response = res;
+        return User.findByEmail(email);
+      })
+      .then(user => {
+        newUser = user;
+        done();
+      })
+      .catch(err => {
+        done(err);
+      });
+    });
+
+    it('should activate the user in database', function() {
+      expect(newUser.activated).to.be.true;
+    });
+
+    it('should return an user token and displayName', function() {
+      expect(response.status).to.equal(200);
+      expect(response.data.token).to.exist;
+      expect(response.data.displayName).to.equal(email);
+    });
+  });
+
+  describe('with mal-formed request data', function() {
 
   });
+
+  describe('duplicate activation', function() {
+    let errorRes, response;
+    const email = correctEmail;
+
+    before(function(done) {
+      const hash = newUser.UUID;
+      axios.patch(`${SERVER_URL}/activateAccount`, { email, hash })
+      .then(res => {
+        response = res;
+        done();
+      })
+      .catch(err => {
+        errorRes = err;
+        done();
+      });
+    });
+
+    it('should return an 409 error', function() {
+      expect(errorRes.status).to.equal(409);
+    });
+  });
+
+  after(function(done) {
+    User.findByEmail(correctEmail).then(user => {
+      user.destroy();
+      done();
+    });
+  });
+
+});
+
+describe('/signIn', function() {
+  let newUser;
+  const correctEmail = 'test@mail.com';
+
+  before(function(done) {
+    axios.post(`${SERVER_URL}/signUp`, {
+      email: correctEmail,
+      password: 'password'
+    })
+    .then(res => {
+      return User.findByEmail(correctEmail);
+    })
+    .then(user => {
+      newUser = user;
+      done();
+    })
+    .catch(err => {
+      done(err);
+    });
+  });
+
+  describe('with non-activated user', function() {
+    let errorRes, response;
+    const email = correctEmail;
+    const password = 'password';
+
+    before(function(done) {
+      axios.post(`${SERVER_URL}/signIn`, { email, password })
+      .then(res => {
+        response = res;
+        done();
+      })
+      .catch(err => {
+        errorRes = err;
+        done();
+      })
+    });
+
+    it('should return an 400 error', function() {
+      expect(errorRes.status).to.equal(400);
+    });
+
+    after(function(done) {
+      axios.patch(`${SERVER_URL}/activateAccount`, {
+        email: correctEmail,
+        hash: newUser.UUID
+      }).then(res => {
+        done();
+      });
+    });
+  });
+
+  describe('with correct request data', function() {
+    let errorRes, response;
+    const email = correctEmail;
+    const password = 'password';
+
+    before(function(done) {
+      axios.post(`${SERVER_URL}/signIn`, { email, password })
+      .then(res => {
+        response = res;
+        done();
+      })
+      .catch(err => {
+        errorRes = err;
+        done();
+      })
+    });
+
+    it('should return user token and displayName', function() {
+      expect(response.status).to.equal(200);
+      expect(response.data.token).to.exist;
+      expect(response.data.displayName).to.equal(email);
+    });
+  });
+
+  describe('with mal-formed request data', function() {
+
+  });
+
+  describe('with non-matching password', function() {
+    let errorRes, response;
+    const email = correctEmail;
+    const password = 'some other password';
+
+    before(function(done) {
+      axios.post(`${SERVER_URL}/signIn`, { email, password })
+      .then(res => {
+        response = res;
+        done();
+      })
+      .catch(err => {
+        errorRes = err;
+        done();
+      })
+    });
+
+    it('should return a 401 error', function() {
+      expect(errorRes.status).to.equal(401);
+    });
+  });
+
+  describe('with non-existing email', function() {
+    let errorRes, response;
+    const email = 'non-existing@mail.com';
+    const password = 'password';
+
+    before(function(done) {
+      axios.post(`${SERVER_URL}/signIn`, { email, password })
+      .then(res => {
+        response = res;
+        done();
+      })
+      .catch(err => {
+        errorRes = err;
+        done();
+      })
+    });
+
+    it('should return a 422 error', function() {
+      expect(errorRes.status).to.equal(422);
+    });
+  });
+
+  after(function(done) {
+    User.findByEmail(correctEmail).then(user => {
+      user.destroy();
+      done();
+    });
+  });
+
+});
+
+describe('/checkEmail', function() {
+  let newUser;
+  const correctEmail = 'test@mail.com';
+
+  before(function(done) {
+    axios.post(`${SERVER_URL}/signUp`, {
+      email: correctEmail,
+      password: 'password'
+    })
+    .then(res => {
+      return User.findByEmail(correctEmail);
+    })
+    .then(user => {
+      newUser = user;
+      done();
+    })
+    .catch(err => {
+      done(err);
+    });
+  });
+
+  describe('with mal-formed request data', function() {
+
+  });
+
+
+  after(function(done) {
+    User.findByEmail(correctEmail).then(user => {
+      user.destroy();
+      done();
+    });
+  });
+
 });
